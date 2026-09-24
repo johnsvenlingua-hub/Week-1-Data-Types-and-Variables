d1dc9e0e137609f54ba60d1acd95ca630324c332
johnsvenlingua-hub <johnsven.lingua@gmail.com>
2026-09-24 11:15:11 +0800

    Create PersonalProfile.java

---
+++ PersonalProfile.java
@@ -0,0 +1,16 @@
+public class PersonalProfile {
+    public static void main(String[] args) {
+        int age = 19;
+        double height = 1.62;
+        char initial = 'L';
+        boolean isStudent = true;
+
+        String fullName = "John Sven Lingua";
+
+        System.out.println("Full Name: " + fullName);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " meters");
+        System.out.println("Initial: " + initial);
+        System.out.println("Student Status: " + isStudent);
+    }
+}
